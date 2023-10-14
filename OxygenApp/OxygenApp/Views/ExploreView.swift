adf4befdd749cc8c2bfe7d8b6068146b516d56a2
Benjamin Price <benjaminprice2310@gmail.com>
2023-10-14 14:42:56 -0700

    view file splitting and explore page switch

---
+++ OxygenApp/OxygenApp/Views/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  OxygenApp
+//
+//  Created by Benjamin Price on 10/14/23.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ OxygenApp/OxygenApp/Views/MyProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  MyProfileView.swift
+//  OxygenApp
+//
+//  Created by Benjamin Price on 10/14/23.
+//
+
+import SwiftUI
+
+struct MyProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyProfileView()
+}
