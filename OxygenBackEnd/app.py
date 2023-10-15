5329c8aee9e0568b4080e9e6ccd84f79e362cd97
ndaeila <ndaeila@uw.edu>
2023-10-14 19:54:18 -0700

    Complete flask backend...

--- OxygenBackEnd/app.py
+++ OxygenBackEnd/app.py
@@ -1,15 +1,39 @@
 import json
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 
 
 # Init Flask app
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
-@app.route("/endpoint", methods = ['POST'])
-def endpoint():
+@app.route("/getJobNodes", methods = ['POST'])
+def getJobNodes():
+    data = request.form
 
-    return None
+    # fake distance checker
+    if (data['latitude'] < "45" and data['latitude'] > "40") and (data['longitude'] > "-120" and data['longitude'] < "-130"):
+
+        with open('data/JobData.json', 'r') as file:
+            data = json.load(file)
+
+        return data
+    else:
+        return []
+
+
+@app.route("/getCourseNodes", methods = ['POST'])
+def getCourseNodes():
+    data = request.form
+
+    # fake distance checker
+    if (data['latitude'] < "45" and data['latitude'] > "40") and (data['longitude'] > "-120" and data['longitude'] < "-130"):
+
+        with open('data/CourseData.json', 'r') as file:
+            data = json.load(file)
+
+        return data
+    else:
+        return []
 
 
 if __name__ == '__main__':
